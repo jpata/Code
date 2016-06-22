c9a816fc702be0af013611da2d04d684d4930814
Joosep Pata <joosep.pata@gmail.com>
2016-06-22 12:34:31 +0200

    apply only on high-jet

--- test/test_json.py
+++ test/test_json.py
@@ -86,6 +86,9 @@ for cfg in confs:
                 print "stopping after {0} jets".format(njet)
                 break
 
+        if njet <= 6:
+            print "skipping event"
+            continue
         leps_p4 = jsev["input"]["selectedLeptonsP4"]
         leps_charge = jsev["input"]["selectedLeptonsCharge"]
         for p4, charge in zip(leps_p4, leps_charge):
